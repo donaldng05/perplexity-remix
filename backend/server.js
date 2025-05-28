579a5381ccd90f3bfdb15d28f89f741fe6689c16
donaldng05 <quyduongnguyen05@gmail.com>
2025-05-28 17:29:06 +0700

    rechecked azure api keys

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
-// backend/server.js
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const express = require('express');
 const app = express();
